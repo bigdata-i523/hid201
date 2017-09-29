fabce884fa7ba845f5d634e3724f36bcae0273b6
arnav <aarnav@iu.edu>
2017-09-28 23:16:47 -0400

    added python code to check README.md format

---
+++ python_stuff/parse_readme.py
@@ -0,0 +1,52 @@
+#use python 2.7.13
+
+##use library pyyaml for reading as yaml
+# install using "pip install pyyaml"
+
+##usage
+# python parse_readme.py <path_to_readme>
+
+import sys
+import yaml
+
+filename = str(sys.argv[1])
+file = open(filename,'r')
+between_markers = False
+
+yaml_lines = []
+yaml_content = []
+
+#read lines between the ``` markers
+print "The lines between ``` markers are :"
+s = ''
+for line in file:
+	if line.startswith("```") and between_markers:
+		between_markers = False
+		yaml_content.append(s)
+		s = ''
+	elif line.startswith("```") and not between_markers:
+		between_markers = True
+	else:
+		if between_markers:
+			yaml_lines.append(line)
+			s += line
+			print line
+
+#check for the tab
+for line in yaml_lines:
+	if line.startswith("\t"):
+		print "ERROR!! in line :"
+		print line, "starts with a tab.. use spaces\n"
+
+
+
+#read content as yaml strings
+#print dictionaries
+#check and display errors
+print "Dictionaries in the file are:"
+try:
+	for data in yaml_content:
+		dictionary = yaml.load(data)
+		print dictionary
+except Exception as e:
+	print e
